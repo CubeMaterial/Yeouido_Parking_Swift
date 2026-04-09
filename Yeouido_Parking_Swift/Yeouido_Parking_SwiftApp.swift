f8b7ee257cf09e9ef62230b82d1f80342a093143
CubeMaterial <mac@MACs-MacBook-Air.local>
2026-04-09 16:31:57 +0900

    다시 올리기

---
+++ Yeouido_Parking_Swift/Yeouido_Parking_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yeouido_Parking_SwiftApp.swift
+//  Yeouido_Parking_Swift
+//
+//  Created by MAC on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct Yeouido_Parking_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
